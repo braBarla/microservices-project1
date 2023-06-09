459d02f0e2670b088e3be3fb902ceb5ba059c558
braBarla <brian.barla@gmail.com>
2023-06-09 11:02:35 +0200

    add enableconfig to eureka service

--- eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
